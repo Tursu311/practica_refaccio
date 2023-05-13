f75793d5f55b068b4702b148b09438906e2c30b4
Tursu <[nil.masso.caba@gmail.com]>
2023-05-13 13:01:55 +0200

    .

---
+++ practica_REFAC/src/Human.java
@@ -0,0 +1,10 @@
+public class Human {
+    private String name;
+    private String age;
+    private String country;
+    private String city;
+    private String street;
+    private String house;
+    private String quarter;
+    
+}
--- practica_REFAC/src/RefactorizedMain.java
+++ practica_REFAC/src/RefactorizedMain.java
@@ -1,155 +1,8 @@
-import java.util.List;
+//REFACT: Delete unused code import java.util.File and import java.util.List;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String p = "Proves";
 
-        int opcio;
-        do {
-            System.out.println("1. ");
-            System.out.println("2. ");
-            System.out.println("3. ");
-            System.out.println("4. ");
-            System.out.println("5. ");
-            System.out.println("0. Acabar");
-
-            opcio = scan.nextInt();
-            scan.nextLine();
-            switch (opcio) {
-                case 1:
-                    System.out.println("intro: ");
-                    int num1 = scan.nextInt();
-                    System.out.println("intro: ");
-                    int num2 = scan.nextInt();
-                    if (max(num1,num2)) {
-                        System.out.println("aaa");
-                    }
-                    else System.out.println("bbb");
-                    break;
-                case 2:
-                    double a=2;
-                    double b=3;
-                    double c=1;
-                    calcEquacioSegongrau(a, b, c);
-                    break;
-                case 3:
-                    List<OrderLineItem> lineItems = null;
-                    Order asd = new Order(lineItems, 5.5);
-                    break;
-                case 0:
-                    break;
-                default:
-                    System.out.println("ATENCIÓ!!! \nHa de ser un valor entre 0 i 5");
-            }
-        } while (opcio != 0);
-    }
-    public static boolean max(int a, int b) {
-        if(a > b) {
-            return true;
-        } else if (a == b) {
-            return false;
-        } else {
-            return false;
-        }
-    }
-    public static void calcEquacioSegongrau(double a, double b, double c) {
-        double D = b * b - 4 * a * c;
-        if (D > 0) {
-            double x1, x2;
-            x1 = (-b - Math.sqrt(D)) / (2 * a);
-            x2 = (-b + Math.sqrt(D)) / (2 * a);
-            System.out.println("x1 = " + x1 + ", x2 = " + x2);
-        }
-        else if (D == 0) {
-            double x;
-            x = -b / (2 * a);
-            System.out.println("x = " + x);
-        }
-        else {
-            System.out.println("Equation has no roots");
-        }
-    }
-    public static class Human {
-        private String name;
-        private String age;
-        private String country;
-        private String city;
-        private String street;
-        private String house;
-        private String quarter;
-        public String obtenirAdrecaCompleta() {
-            StringBuilder result = new StringBuilder();
-            return result
-                    .append(country)
-                    .append(", ")
-                    .append(city)
-                    .append(", ")
-                    .append(street)
-                    .append(", ")
-                    .append(house)
-                    .append(" ")
-                    .append(quarter).toString();
-        }
-    }
-
-    public static class Order {
-        private List<OrderLineItem> lineItems;
-        private double taxRate;
-
-        public Order(List<OrderLineItem> lineItems, double taxRate) {
-            this.lineItems = lineItems;
-            this.taxRate = taxRate;
-        }
-
-        public double calculateTotalPrice() {
-            double subtotal = 0.0;
-            for (OrderLineItem item : lineItems) {
-                subtotal += item.getPrice();
-            }
-            double tax = subtotal * taxRate;
-            return subtotal + tax;
-        }
-    }
-
-     public class OrderLineItem {
-        private String productName;
-        private int quantity;
-        private double price;
-
-        public OrderLineItem(String productName, int quantity, double price) {
-            this.productName = productName;
-            this.quantity = quantity;
-            this.price = price;
-        }
-        public double getPrice() {
-            return price * quantity;
-        }
-    }
-
-    public class Customer {
-        private String firstName;
-        private String lastName;
-
-        public Customer(String firstName, String lastName) {
-            this.firstName = firstName;
-            this.lastName = lastName;
-        }
-
-        public String getFullName() {
-            return firstName + " " + lastName;
-        }
-    }
-}
-
-//Refactorize the abobe code and add //REFACT: (refactoring type)
-
-//REFACT: Delete unused code import java.util.File;
-import java.util.List;
-import java.util.Scanner;
-
-public class Main {
+public class RefactorizedMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         //REFACT: Delete unused code String p = "Proves";
@@ -160,26 +13,40 @@ public class Main {
         //REFACT: Delete unused code scan.nextLine();
         switch (opcio) {
             case 1:
-                calcularmesgran();
+                System.out.println("Introdueix el primer nombre: ");
+                int num1 = scan.nextInt();
+                System.out.println("Introdueix el segon nombre: ");
+                int num2 = scan.nextInt();
+                calcularmesgran(num1, num2);
                 break;
             case 2:
-                
+                //REFACT: Added user input for a, b, c
+                System.out.println("Introdueix el coeficient a: ");
+                double a = scan.nextDouble();
+                System.out.println("Introdueix el coeficient b: ");
+                double b = scan.nextDouble();
+                System.out.println("Introdueix el coeficient c: ");
+                double c = scan.nextDouble();
+                calcEquacioSegongrau(a, b, c);
+                break;
+            case 3:
+                System.out.println("Introdueix el nom del client: ");
+                //Comprovem si el client existeix
+                String nom = scan.nextLine();
+                ComprovarClient(nom);
+
 
 
 
     }
 
-    public menuprincipal() {
+    public static void menuprincipal() { 
         System.out.println("1. Introduir dos nombres i mostrar el més gran");
         System.out.println("2. Introduir els coeficients d'una equació de segon grau i mostrar les seves arrels");
 
     }
 
-    public calcularmesgran() { //RE
-        System.out.println("Introdueix el primer nombre: ");
-        int num1 = scan.nextInt();
-        System.out.println("Introdueix el segon nombre: ");
-        int num2 = scan.nextInt();
+    public static void calcularmesgran(int num1, int num2) { //REFACT: Changed method name to calcularmesgran() 
         if (num1 > num2) {
             System.out.println("El número més gran és: " + num1);
         } else if (num1 == num2) {
@@ -188,4 +55,22 @@ public class Main {
             System.out.println("El número més gran és: " + num2);
         }
     }
+
+    public static void calcEquacioSegongrau(double a, double b, double c) {
+        double D = b * b - 4 * a * c;
+        if (D > 0) {
+            double x1 = (-b + Math.sqrt(D)) / (2 * a);
+            double x2 = (-b - Math.sqrt(D)) / (2 * a);
+            System.out.println("Les arrels de l'equació són: " + x1 + " i " + x2);
+        } else if (D == 0) {
+            double x = -b / (2 * a);
+            System.out.println("L'arrel de l'equació és: " + x);
+        } else {
+            System.out.println("L'equació no té solució");
+        }
+    }
+
+    public static void crearClient(String nom) 
+    }
+
 }
